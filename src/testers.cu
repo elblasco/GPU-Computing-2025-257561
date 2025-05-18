ccc0c1c2fb5065ae98239199c290d396c582b53f
elblasco <alessio.blascovich@studenti.unitn.it>
2025-05-18 20:37:34 +0200

    fix: cpu timing ms instead of ns

--- src/testers.cu
+++ src/testers.cu
@@ -1,7 +1,6 @@
 #include <chrono>
 #include <ratio>
 #include <cmath>
-#include <thread>
 
 #include "include/cpu.h"
 #include "include/utils.h"
@@ -12,6 +11,7 @@ NUM_TYPE* test_spmv_cpu(const IDX_TYPE *row_indices, const IDX_TYPE *col_indices
                    const IDX_TYPE num_rows, const size_t nnz) {
   double times[NUM_TEST];
   double flops[NUM_TEST];
+  double bandwidth[NUM_TEST];
   NUM_TYPE *resulting_array;
   
   for (size_t i = 0; i < NUM_TEST; ++i) {
@@ -21,26 +21,31 @@ NUM_TYPE* test_spmv_cpu(const IDX_TYPE *row_indices, const IDX_TYPE *col_indices
     spmv_cpu(row_indices, col_indices, val, arr, nnz, resulting_array);
     auto end = std::chrono::high_resolution_clock::now();
 
-	std::chrono::duration<double, std::micro> duration_ms = end - start;
+	std::chrono::duration<double, std::milli> duration_ms = end - start;
 	
     double elapsed_ms = duration_ms.count();
 	flops[i] = flops_counter(nnz, elapsed_ms);
 	times[i] = elapsed_ms;
+	bandwidth[i] = (nnz * sizeof(NUM_TYPE) * MEMEORY_RW / elapsed_ms) / 1e12;
 	
 	if(i < NUM_TEST - 1)
 	  delete[] resulting_array;
   }
-
+  
+  double times_mu = mu_fn(times, NUM_TEST);
+  double times_sigma = sigma_fn(times, times_mu, NUM_TEST);
+  
+  printf("This kernel executed with an average of %lf ms with std.dev. of %lf ms\n", times_mu, times_sigma);
+  
   double flops_mu = mu_fn(flops, NUM_TEST);
   double flops_sigma = sigma_fn(flops, flops_mu, NUM_TEST);
   
   printf("This kernel produced an average of %lf GFLOPS with std.dev. of %lf GFLOPS\n", flops_mu, flops_sigma);
 
-  double times_mu = mu_fn(times, NUM_TEST);
-  double times_sigma = sigma_fn(times, times_mu, NUM_TEST);
+  double bandwidth_mu = mu_fn(bandwidth, NUM_TEST);
+  double bandwidth_sigma = sigma_fn(bandwidth, bandwidth_mu, NUM_TEST);
   
-  printf("This kernel executed with an average of %lf ms with std.dev. of %lf ms\n", times_mu, times_sigma);
-
+  printf("This kernel produced an avergare bandwidth of %lf GB/ms with std.dev. of %lf GB/ms\n", bandwidth_mu, bandwidth_sigma);
   return resulting_array;
 }
 
@@ -76,9 +81,6 @@ NUM_TYPE* test_spmv_gpu(gpu_kernel kernel, const IDX_TYPE *row_indices,
 
     float milliseconds = 0;
     cudaCheckError(cudaEventElapsedTime(&milliseconds, start, stop));
-
-	using namespace std::chrono_literals;
-	std::this_thread::sleep_for(10000ms);
 	
     flops[i] = flops_counter(nnz, milliseconds);
     times[i] = milliseconds;
@@ -88,15 +90,20 @@ NUM_TYPE* test_spmv_gpu(gpu_kernel kernel, const IDX_TYPE *row_indices,
 	  cudaCheckError(cudaFree(resulting_array));
   }
   
+  double times_mu = mu_fn(times, NUM_TEST);
+  double times_sigma = sigma_fn(times, times_mu, NUM_TEST);
+  
+  printf("This kernel executed with an average of %lf ms with std.dev. of %lf ms\n", times_mu, times_sigma);
+  
   double flops_mu = mu_fn(flops, NUM_TEST);
   double flops_sigma = sigma_fn(flops, flops_mu, NUM_TEST);
   
   printf("This kernel produced an average of %lf GFLOP/s with std.dev. of %lf GFLOP/s\n", flops_mu, flops_sigma);
 
-  double times_mu = mu_fn(times, NUM_TEST);
-  double times_sigma = sigma_fn(times, times_mu, NUM_TEST);
+  double bandwidth_mu = mu_fn(bandwidth, NUM_TEST);
+  double bandwidth_sigma = sigma_fn(bandwidth, bandwidth_mu, NUM_TEST);
   
-  printf("This kernel executed with an average of %lf ms with std.dev. of %lf ms\n", times_mu, times_sigma);
+  printf("This kernel produced an avergare bandwidth of %lf GB/s with std.dev. of %lf GB/s the theoretical maximun is 933 GB/s\n", bandwidth_mu, bandwidth_sigma);
   
   cudaCheckError(cudaEventDestroy(start));
   cudaCheckError(cudaEventDestroy(stop));
