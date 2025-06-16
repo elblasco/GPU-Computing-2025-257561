a54bb055b6baf0b4c2fc6ab10a3fd0fe7bd15a22
elblasco <alessio.blascovich@studenti.unitn.it>
2025-06-16 10:19:21 +0200

    del; submodule and manual download

--- deliverable-02/env.sh
+++ deliverable-02/env.sh
@@ -8,15 +8,35 @@ PUR='\033[0;35m'
 GRE='\033[0;32m'
 NC='\033[0m' # No Color
 
-git submodule update --init
-
-sed -i 's|url = git@github.com:HicrestLaboratory/distributed_mmio.git|url = https://github.com/HicrestLaboratory/distributed_mmio|' MtxMan/.gitmodules 
-
 git submodule update --init --recursive
 
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r ./MtxMan/requirements.txt
+# Create the dataset directory if it doesn't exist
+mkdir -p dataset
+
+# Change into the dataset directory
+cd dataset
+
+# List of URLs to download
+urls=(
+  "https://suitesparse-collection-website.herokuapp.com/MM/Oberwolfach/bone010.tar.gz"
+  "https://suitesparse-collection-website.herokuapp.com/MM/Dziekonski/dielFilterV3real.tar.gz"
+  "https://suitesparse-collection-website.herokuapp.com/MM/MAWI/mawi_201512020330.tar.gz"
+  "https://suitesparse-collection-website.herokuapp.com/MM/Schenk/nlpkkt160.tar.gz"
+)
+
+# Download and extract each file
+for url in "${urls[@]}"; do
+  file=$(basename "$url")
+  echo "Downloading $file..."
+  curl -O "$url" || wget "$url"
+  
+  echo "Extracting $file..."
+  tar -xzf "$file"
+done
+
+echo "All files downloaded and extracted."
+
+cd ..
 
 export BIN=bin/coo-mul
 export ITERATIONS=15
@@ -26,8 +46,6 @@ if [[ ! -z $1 ]]; then
 else
     export MTX_PATH="./datasets"
 fi
-
-python3 ./MtxMan/scripts/sync_datasets.py --binary-mtx
 
 # Read graph paths from matrices_list.txt in each subfolder
 GRAPHS_SMALL_D=()
