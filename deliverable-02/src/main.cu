5b8a2ea53552c440522397b182aaf09c020220de
elblasco <alessio.blascovich@studenti.unitn.it>
2025-06-16 21:27:45 +0200

    add: barebone main

--- deliverable-02/src/main.cu
+++ deliverable-02/src/main.cu
@@ -1,3 +1,24 @@
-int main() {
+#include "../distributed_mmio/include/mmio.h"
+#include "../distributed_mmio/include/mmio_utils.h"
+
+#include "../include/colours.h"
+#include "../include/utils.cuh"
+#include "../include/cli.hpp"
+
+int main(int argc, char **argv) {
+  Cli_Args args;
+  init_cli();
+  if (parse_args(argc, argv, &args) != 0) {
+    return -1;
+  }
+  CPU_TIMER_INIT(MTX_read);
+
+  COO_local<uint64_t, double> *coo_matrix = Distr_MMIO_COO_local_read<uint64_t, double>(args.filename);
+
+  CPU_TIMER_STOP(MTX_read);
+
+  printf("\n[OUT] MTX file read time: %f ms\n", CPU_TIMER_ELAPSED(MTX_read));
+
+  printf("Graph size: %.3fM vertices, %.3fM edges\n", coo_matrix->nrows / 1e6, coo_matrix->nnz / 1e6);
   return 0;
 }
